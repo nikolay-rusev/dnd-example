d7321dd70aa18d76ed62da58b11b5d641c1629d5
Nikolay Rusev <nikolay.z.rusev@gmail.com>
2025-05-12 16:30:18 +0300

    feat: add scrolling options

--- src/DragNDrop.js
+++ src/DragNDrop.js
@@ -26,12 +26,14 @@ function SortableItem({ id, activeId, dummy }) {
 
     const itemStyle = dummy ? dummyItemStyle : calcItemStyle({ activeId, transform });
 
+    const dragItemId = dummy ? null : `drag-item-${id}`;
+
     return (
-        <div ref={setNodeRef} {...attributes} style={itemStyle}>
+        <div ref={setNodeRef} {...attributes} style={itemStyle} data-id={dragItemId}>
             <div {...listeners} className={"drag-handle"} style={dragHandleStyle}>
                 ☰
             </div>
-            {id}
+            Item {id}
         </div>
     );
 }
@@ -42,6 +44,23 @@ export default function DragNDrop() {
     const containerRef = useRef(null);
     const [topFillHeight, setTopFillHeight] = useState(0);
     const [bottomFillHeight, setBottomFillHeight] = useState(0);
+
+    const scrollActiveElementIntoView = (id) => {
+        document.querySelector(`[data-id=drag-item-${id}]`)?.scrollIntoView({
+            behavior: "smooth",
+            block: "center"
+        });
+    };
+
+    const scrollAfterDragEnd = (event) => {
+        // Scroll to the final position
+        setTimeout(() => {
+            document.querySelector(`[data-id=drag-item-${event.active.id}]`)?.scrollIntoView({
+                behavior: "smooth",
+                block: "center"
+            });
+        }, 200); // Slight delay to allow transition
+    };
 
     const resetFillHeights = () => {
         setTimeout(() => {
@@ -71,6 +90,8 @@ export default function DragNDrop() {
 
             setTopFillHeight(leftoverHeight * proportion);
             setBottomFillHeight(leftoverHeight * (1 - proportion));
+
+            // scrollActiveElementIntoView(event.active.id);
         }, TIMEOUT); // Ensuring transition has completed
     };
 
@@ -92,6 +113,8 @@ export default function DragNDrop() {
             setItems((prev) =>
                 arrayMove(prev, prev.indexOf(event.active.id), prev.indexOf(event.over.id))
             );
+
+            // scrollAfterDragEnd(event);
         }
     };
 
@@ -118,7 +141,11 @@ export default function DragNDrop() {
             <div className="top-fill" style={{ height: topFillHeight }}></div>
             <div id="dnd-context-container" className="dnd-context-container">
                 <div className="actual-container" style={{ position: "relative" }}>
-                    <DndContext onDragStart={handleDragStart} onDragEnd={handleDragEnd}>
+                    <DndContext
+                        onDragStart={handleDragStart}
+                        onDragEnd={handleDragEnd}
+                        autoScroll={{ layoutShiftCompensation: false }}
+                    >
                         {children}
                     </DndContext>
                 </div>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,19 +1,6 @@
 export const TIMEOUT = 0;
 
-export const dragItemsArray = [
-    "Item 1",
-    "Item 2",
-    "Item 3",
-    "Item 4",
-    "Item 5",
-    "Item 6",
-    "Item 7",
-    "Item 8",
-    "Item 9",
-    "Item 10",
-    "Item 11",
-    "Item 12"
-];
+export const dragItemsArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
 export const dummyContainerStyle = {
     visibility: "hidden",
