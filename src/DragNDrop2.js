34334690ee870ca9678baca24eb9b56db8105e9a
Nikolay Rusev <nikolay.z.rusev@gmail.com>
2025-05-14 17:38:43 +0300

    add dnd2

--- src/DragNDrop2.js
+++ src/DragNDrop2.js
@@ -2,20 +2,8 @@ import { DragDropProvider } from "@dnd-kit/react";
 import { useSortable } from "@dnd-kit/react/sortable";
 import { move } from "@dnd-kit/helpers";
 import { SortableContext } from "@dnd-kit/sortable";
-import { DndContext, DragOverlay, MeasuringStrategy, pointerWithin } from "@dnd-kit/core";
-import {
-    restrictToParentElement,
-    restrictToVerticalAxis,
-    snapCenterToCursor
-} from "@dnd-kit/modifiers";
-import {
-    dragHandleStyle,
-    dragItemsArray,
-    dummyItemStyle,
-    shrinkContainerStyle,
-    TIMEOUT
-} from "./utils/constants";
-import { calcItemStyleForDND2, getActualElementHeight, scrollAfterDragEnd } from "./utils/helpers";
+import { dragItemsArray, dummyItemStyle, shrinkContainerStyle, TIMEOUT } from "./utils/constants";
+import { calcItemStyleForDND2, getActualElementHeight } from "./utils/helpers";
 import { useRef, useState } from "react";
 
 function SortableItem({ id, index, isDragging, dummy, last, className }) {
@@ -72,11 +60,8 @@ export default function DragNDrop2() {
 
         // Get the context container height after shrinking (assuming transitions complete)
         setTimeout(() => {
-            // Adjust mouse Y based on scrolling
-            const scrollOffset = window.scrollY;
-
             const activatorEvent = event.operation.activatorEvent;
-            const mouseY = activatorEvent.clientY;
+            const mouseY = activatorEvent?.clientY;
             // get element
             const draggedElement = activatorEvent?.srcElement;
             const topOfDraggedElement = draggedElement.getBoundingClientRect().top;
@@ -97,17 +82,10 @@ export default function DragNDrop2() {
             const topCompensation =
                 topOfDraggedElement - topOfShrinkEl + adjust - shrinkElementHeight * ratio;
 
-            console.log("initialHeight: ", initialHeight);
-            console.log("shrinkContainerHeight: ", shrinkContainerHeight);
-            console.log("leftoverHeight: ", leftoverHeight);
-
-            // easy
             const bottomCompensation = leftoverHeight - topCompensation;
 
             setTopFillHeight(topCompensation);
             setBottomFillHeight(bottomCompensation);
-
-            // scrollActiveElementIntoView(event?.active?.id);
         }, TIMEOUT); // Ensuring transition has completed
     };
 
@@ -120,45 +98,28 @@ export default function DragNDrop2() {
 
     const handleDragEnd = (event) => {
         setIsDragging(false);
-        // Smooth reset of fill heights
-        resetFillHeights();
 
-        const draggedItem = event.operation.source;
+        // const draggedItem = event.operation.source;
+        // reset of fill heights
+        resetFillHeights();
 
         return setItems((items) => move(items, event));
     };
 
     const getDraggableItems = ({ items, dummy = false }) => {
         return (
-            <>
-                <SortableContext items={items}>
-                    {items.map((id, index) => (
-                        <SortableItem
-                            key={id}
-                            id={id}
-                            isDragging={isDragging}
-                            index={index}
-                            dummy={dummy}
-                            last={index === items.length - 1}
-                        />
-                    ))}
-                </SortableContext>
-                {/*<DragOverlay*/}
-                {/*    modifiers={[*/}
-                {/*        snapCenterToCursor,*/}
-                {/*        restrictToVerticalAxis,*/}
-                {/*        restrictToParentElement*/}
-                {/*    ]}*/}
-                {/*>*/}
-                {/*    {activeId ? (*/}
-                {/*        <SortableItem*/}
-                {/*            id={activeId}*/}
-                {/*            activeId={activeId}*/}
-                {/*            className={"drag-overlay"}*/}
-                {/*        />*/}
-                {/*    ) : null}*/}
-                {/*</DragOverlay>*/}
-            </>
+            <SortableContext items={items}>
+                {items.map((id, index) => (
+                    <SortableItem
+                        key={id}
+                        id={id}
+                        isDragging={isDragging}
+                        index={index}
+                        dummy={dummy}
+                        last={index === items.length - 1}
+                    />
+                ))}
+            </SortableContext>
         );
     };
     const children = getDraggableItems({ items });
