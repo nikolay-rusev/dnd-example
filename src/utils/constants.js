b5bf969b7c78dd4891918ee836122e2dd96d36af
Nikolay Rusev <nikolay.z.rusev@gmail.com>
2025-05-15 10:56:03 +0300

    debug

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -8,7 +8,7 @@ export const SHRUNK_HEIGHT = 12;
 export const dragItemsArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 export const shrinkContainerStyle = {
-    visibility: "hidden",
+    visibility: "visible",
     position: "absolute",
     top: 0,
     right: 0
